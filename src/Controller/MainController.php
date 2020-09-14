3e14b93c852281e03cc5c155a947f4646c4e1669
philippe <spartitus>
2020-09-14 06:42:35 +0200

    create home page and legal-notice

---
+++ src/Controller/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/legal-notice", name="mentions")
+     */
+    public function mentions()
+    {
+        return $this->render('main/mentions.html.twig');
+    }
+}
